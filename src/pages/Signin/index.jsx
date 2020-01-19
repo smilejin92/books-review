ba40b70a430f0d0343ffc31eaa90c14dfec16421
smilejin92 <smilejin92@gmail.com>
2020-01-20 03:50:18 +0900

    styled login page with styled-components

---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Row, Col } from 'antd';
+import SigninBg from './SigninBg';
+import SigninForm from './SigninForm';
+
+const StyledRow = styled(Row).attrs(() => ({
+  type: 'flex',
+  align: 'middle',
+}))`
+  height: 100vh;
+`;
+
+const StyledCol = styled(Col).attrs(() => ({
+  span: 24,
+}))``;
+
+const StyledTitle = styled.div`
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin: 0;
+`;
+
+const StyledSubTitle = styled(StyledTitle)`
+  font-size: 27px;
+`;
+
+const StyledUnderline = styled.div`
+  width: 200px;
+  height: 8px;
+  margin: 20px auto 0 auto;
+  background: linear-gradient(to right, #803b32, #ddb49b);
+`;
+
+const StyledContent = styled(Row).attrs(() => ({
+  type: 'flex',
+}))`
+  width: 800px;
+  background-color: #f3f7f8;
+  margin: 50px auto 0 auto;
+`;
+
+export default function Signin() {
+  return (
+    <StyledRow>
+      <StyledCol>
+        <StyledTitle>Books Review</StyledTitle>
+        <StyledSubTitle>
+          Please Share Your Opinion on Web Development Books.
+        </StyledSubTitle>
+        <StyledUnderline />
+        <StyledContent>
+          <SigninBg />
+          <SigninForm />
+        </StyledContent>
+      </StyledCol>
+    </StyledRow>
+  );
+}
